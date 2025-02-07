e6572bbc3affa280d469ad8bcbaad339b4bad4ae
Ant1GraV1TY <130571226+Hosh1noAi@users.noreply.github.com>
2025-02-07 21:24:07 +0800

    Update Dlsite自动登录.py

--- Dlsite自动登录.py
+++ Dlsite自动登录.py
@@ -10,8 +10,8 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get('https://login.dlsite.com/login?user=self')
 
 # 查找元素并输入信息
-driver.find_element(By.NAME, 'login_id').send_keys('3592547548@qq.com')
-driver.find_element(By.NAME, 'password').send_keys('lzw031218')
+driver.find_element(By.NAME, 'login_id').send_keys('用户id')
+driver.find_element(By.NAME, 'password').send_keys('密码')
 driver.find_element(By.XPATH, '//button[@type="submit"]').click()
 
 # 验证登录（根据页面内容调整）
